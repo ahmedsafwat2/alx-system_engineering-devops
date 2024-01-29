ae5e1b7bfcab93cd2051dfbca26b4c052f4ee406
Your Name <you@example.com>
2024-01-29 14:18:21 +0200

    try

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,10 @@
-file_line { 'Turn off passwd auth':
+file_line {	'Turn off passwd auth':
 	ensure => 'present',
 	path   => '/etc/ssh/ssh_config',
-	line   => '    PasswordAuthentication no',
+	line   => 'PasswordAuthentication no',
 }
-file_line { 'Declare identity file':
+file_line {	'Declare identity file':
 	ensure => 'present',
 	path   => '/etc/ssh/ssh_config',
-	line   => '    IdentityFile ~/.ssh/school',
+	line   => 'IdentityFile ~/.ssh/school',
 }
