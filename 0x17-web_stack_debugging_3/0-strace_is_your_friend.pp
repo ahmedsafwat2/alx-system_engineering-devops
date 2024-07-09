cbaf0792dec41c56f5eb68a64ab345928e489902
ahmed <ahmed>
2024-07-09 04:45:07 -0700

    debug

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
-# fix "phpp" extensions" to "php"
+# Creat a manifest that fix all termintion of phpp.
 
-exec{'fix-wordpress':
-	command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-	path    => '/usr/local/bin/:/bin/'
+exec { 'fix_phpp':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => ['/bin', '/usr/bin/', '/usr/loca/bin/'],
 }
